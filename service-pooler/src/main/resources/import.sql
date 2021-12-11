e12527230fdbcbf4376ee6031a84f9357f6929c6
Adrian Cozac <adrian.cozac@itiviti.com>
2021-12-11 13:24:42 +0200

    Update import scripts and db config
    Change-Id: I3b86b50edb9ab590d99c9bc71aaf4b5e1415bbc3

--- service-pooler/src/main/resources/import.sql
+++ service-pooler/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO service(id, name, url, status)
-VALUES (nextval('hibernate_sequence'), 'Quarkus super sonic', 'https://quarkus.io/', 'VALID');
-INSERT INTO service(id, name, url, status)
-VALUES (nextval('hibernate_sequence'), 'Cozac git hub', 'https://github.com/cozaca/service-monitor', 'VALID');
-INSERT INTO service(id, name, url, status)
-VALUES (nextval('hibernate_sequence'), 'Cozac Linked profile', 'https://www.linkedin.com/in/cozac-adrian-937b136b/', 'VALID');
+INSERT INTO service(id, name, url, status, creationTime)
+VALUES (nextval('hibernate_sequence'), 'Quarkus super sonic', 'https://quarkus.io/', 'VALID', '20211112');
+INSERT INTO service(id, name, url, status, creationTime)
+VALUES (nextval('hibernate_sequence'), 'Cozac git hub', 'https://github.com/cozaca/service-monitor', 'VALID', '20211112');
+INSERT INTO service(id, name, url, status, creationTime)
+VALUES (nextval('hibernate_sequence'), 'Cozac Linked profile', 'https://www.linkedin.com/in/cozac-adrian-937b136b/', 'VALID', '20211112');
