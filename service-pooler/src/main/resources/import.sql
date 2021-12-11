fcef127e5d5590515817b6eb43251378ae236eb7
Adrian Cozac <adrian.cozac@itiviti.com>
2021-12-11 13:01:55 +0200

    Add service infrastructure
    Change-Id: Ic9d45cd24dc49437decf48e2df759c7812c4a3f9

---
+++ service-pooler/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO service(id, name, url, status)
+VALUES (nextval('hibernate_sequence'), 'Quarkus super sonic', 'https://quarkus.io/', 'VALID');
+INSERT INTO service(id, name, url, status)
+VALUES (nextval('hibernate_sequence'), 'Cozac git hub', 'https://github.com/cozaca/service-monitor', 'VALID');
+INSERT INTO service(id, name, url, status)
+VALUES (nextval('hibernate_sequence'), 'Cozac Linked profile', 'https://www.linkedin.com/in/cozac-adrian-937b136b/', 'VALID');
